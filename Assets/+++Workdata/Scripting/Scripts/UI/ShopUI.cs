343ce6f7699f2daeee13a2348ce2fab296d176b5
Martin <mviegehls22@gmail.com>
2025-06-11 21:29:42 +0200

    balloncart animation works now

--- Assets/+++Workdata/Scripting/Scripts/UI/ShopUI.cs
+++ Assets/+++Workdata/Scripting/Scripts/UI/ShopUI.cs
@@ -292,7 +292,7 @@ public class ShopUI : MonoBehaviour
         if (hoverOnlyButton == null) 
             return;
 
-        if (clickable)
+        if (!clickable)
         {
             if (GameInputManager.Instance.mouseIsLastUsedDevice)
             {
